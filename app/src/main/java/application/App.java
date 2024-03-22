c5e92ff104c979bcbdf308e0307747d850b3289d
gaabrielz1 <162649142+gaabrielz1@users.noreply.github.com>
2024-03-22 01:32:32 +0000

    ultima_aula

--- app/src/main/java/application/App.java
+++ app/src/main/java/application/App.java
@@ -1,6 +1,8 @@
 package application;
 
+import application.model.Compromisso;
 import application.model.Tarefa;
+import java.util.Calendar;
 
 public class App {
     public static void main(String[] args) {
@@ -12,5 +14,38 @@ public class App {
 
         Tarefa t2 = new Tarefa("Segunda Tarefa");
         System.out.println(t2.getDetalhes());
+        
+        Compromisso c = new Compromisso();
+        c.setDescricao("Periodo de Provas");
+        Calendar dtInit = Calendar.getInstance();
+        dtInit.set(2024, 3, 1);
+        Calendar dtFim = Calendar.getInstance();
+        dtFim.set(2024, 3, 8);
+
+        c.setDataInicial(dtInit);
+        c.setDataFinal(dtFim);
+        
+        System.out.println(c.getDetalhes());
+    }
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(c.getDetalhes());
+
+        Compromisso c2 = new Compromisso();
+        c2.setDescrica("Férias");
+        c2.setDataInicial(1, 7, 2024);
+        c2.setDataFinal(30, 7, 2024);
+
+        System.out.println(c2.getDetalhes());
+
+        ArrayList<Atividade> lista = new Arraylist<Atividade>();
+        lista.add(t);
+        lista.add(2);
+
+        System.out.println("------------[Lista]------------");
+        for(Atividade a : lista) {
+            System.out.println(a.getDetalhes());
+        }
     }
 }
--- app/src/main/java/application/model/Compromisso.java
+++ app/src/main/java/application/model/Compromisso.java
@@ -22,5 +22,20 @@ public class Compromisso extends AbstractAtividade {
     public String getDetalhes(){
         return "[c]" + this.getDescricao() + " :: " + this.getDataInicial().getTime() + " :: " + this.getDataFinal().getTime();
     }
-}
 
+    public class Compromisso extends AbstractAtividade{
+        public String getDetalhes() {
+     }
+    
+    public void setDataInicial(int dia, int mes, int ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(ano, (mes - 1), dia);
+        this.setDataInicial(cal);
+    }
+    public void setDataFinal(int dia, int mes, int ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(ano, (mes - 1), dia);
+        this.setDataFinal(cal);
+}
+    
+    }
